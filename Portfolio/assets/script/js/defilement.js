673c8e7d105683abbc71a139349ca008f9d8fdcc
Tontico <144102137+Tontico@users.noreply.github.com>
2023-10-16 12:24:50 +0200

    Update defilement.js

--- Portfolio/assets/script/js/defilement.js
+++ Portfolio/assets/script/js/defilement.js
@@ -1,21 +1,25 @@
-const ratio = .1;
-
-const options = { //permet de definir notre observer
-    root: null,
-    rootMargin: '0px',
-    threshold: ratio //a quel moment va etre detecter
+const threshold = .1
+const options = {
+  root: null,
+  rootMargin: '0px',
+  threshold
 }
 
-const handleIntersect = function(entries, observer){
-    entries.forEach(function(entry) {
-        if(entry.intersectionRatio > ratio) {
-            entry.target.classList.add('reveal-visible');
-            observer.unobserve(entry.target);
-        } 
-    });
+const handleIntersect = function (entries, observer) {
+  entries.forEach(function (entry) {
+    if (entry.intersectionRatio > threshold) {
+      entry.target.classList.remove('reveal')
+      observer.unobserve(entry.target)
+    }
+  })
 }
-const observer = new IntersectionObserver(handleIntersect, options)
-document.querySelectorAll('.reveal').forEach(function (r){
-    observer.observe(r);
-});
 
+document.documentElement.classList.add('reveal-loaded')
+
+window.addEventListener("DOMContentLoaded", function () {
+  const observer = new IntersectionObserver(handleIntersect, options)
+  const targets = document.querySelectorAll('.reveal')
+  targets.forEach(function (target) {
+    observer.observe(target)
+  })
+})
